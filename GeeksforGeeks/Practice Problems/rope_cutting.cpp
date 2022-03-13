08fb45cd7d12d283dae8c0a6e9421a77fbef99d8
Vishwas Tyagi <vishwast8126@gmail.com>
2022-03-13 13:19:48 +0530

    add problem

---
+++ GeeksforGeeks/Practice Problems/rope_cutting.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+        vector<int> RopeCutting(int arr[], int n)
+        {
+            vector<int> res;
+            
+            sort(arr, arr+n);
+            
+            int x = arr[0];
+            
+            for(int i = 0; i < n; i++) {
+                if(arr[i] != x) {
+                    res.push_back(n - i);
+                    x = arr[i];
+                }
+            }
+            return res;
+        }
+};
+
+// { Driver Code Starts.
+int main()
+{   
+    int T;
+    cin>> T;
+    while(T--)
+    {
+       	int n;
+       	cin>>n;
+       	int arr[n+1];
+       	for(int i=0;i<n;i++)
+       	   cin>>arr[i];
+        
+        vector<int> v;
+        Solution ob;
+        v = ob.RopeCutting(arr, n);
+        for(int i : v){
+            cout << i << " ";
+        }
+        cout << endl;
+        
+        
+    }
+  return 0;
+}  // } Driver Code Ends
