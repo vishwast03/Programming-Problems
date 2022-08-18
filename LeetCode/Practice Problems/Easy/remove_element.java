732a707718caaa56864077fe07fb372e066f458b
Vishwas Tyagi <vishwast8126@gmail.com>
2022-08-18 19:10:29 +0530

    18 Aug, 2022

---
+++ LeetCode/Practice Problems/Easy/remove_element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0, j = 0;
+        while(j < nums.length) {
+            if(nums[j] == val) {
+                j++;
+            }
+            else {
+                nums[i] = nums[j];
+                i++;
+                j++;
+            }
+        }
+        return i;
+    }
+}
