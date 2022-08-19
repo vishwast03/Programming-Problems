12e7642dab747612f23d573c5a0bb2de67b93fca
Vishwas Tyagi <vishwast8126@gmail.com>
2022-08-19 12:31:39 +0530

    19 Aug, 2022

---
+++ LeetCode/Practice Problems/Easy/search_insert_position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        int mid = start + (end - start) / 2;
+        
+        while(start <= end) {
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+            
+            mid = start + (end - start) / 2;
+        }
+        
+        return start;
+    }
+}
